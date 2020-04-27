76401aee019b38f97d750fac92b7fc9a8ec3404f
KoeusIss <sebri.issam@gmail.com>
2020-04-27 15:04:20 +0100

    Commit the nineth task

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str.split()[5]+" "+str.split()[6]+" "+str.split()[12]+" "+str.split()[0]
+print(str)
