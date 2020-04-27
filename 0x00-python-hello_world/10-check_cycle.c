6e87cce245eee6035eea78c8cd5cf8ea4914db01
KoeusIss <sebri.issam@gmail.com>
2020-04-27 16:05:49 +0100

    Commit the eleventh task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: a pointer to the list
+ *
+ * Return: (1) if there is a cycle
+ * ------- (0) otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast;
+	listint_t *slow;
+
+	slow = list;
+	fast = list->next;
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
