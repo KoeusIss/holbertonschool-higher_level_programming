ebdafc8fb28cc606e24f130ae98323b76ce1d991
KoeusIss <sebri.issam@gmail.com>
2020-04-28 14:40:25 +0100

    fix the shbang

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def uppercase(str):
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
             cc = ord(c) - 32
         else:
             cc = ord(c)
-        print("{:c}".format(cc), end='')
-    print("{}".format("\n"), end='')
+        print("{}".format(cc), end='')
+    print(end='\n')
