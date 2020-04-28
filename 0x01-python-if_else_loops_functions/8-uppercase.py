65e1df5280b2468bf1812e52378da4a70be410db
KoeusIss <sebri.issam@gmail.com>
2020-04-28 13:53:57 +0100

    Fix the 9th task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,4 @@
 def uppercase(str):
-    """
-    signature: uppercase(str)
-    Docstring: Transforms a string into uppercase
-    str: string to be converted
-    """
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
             cc = chr(ord(c) - 32)
