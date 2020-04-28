1710319cef5a49eb3daf7fd86e197b2407d096a8
KoeusIss <sebri.issam@gmail.com>
2020-04-28 17:15:28 +0100

    Commit the second advanced task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(astr, n):
+    i = 0
+    c_str = ""
+    for c in astr:
+        if i == n:
+            c_str = c_str + ''
+        else:
+            c_str = c_str + c
+        i = i + 1
+    print(c_str)
+    return c_str
