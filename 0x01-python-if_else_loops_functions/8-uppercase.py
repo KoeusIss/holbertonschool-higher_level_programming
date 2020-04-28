aa2b600581aa0e4538215dd1565558d461412eb0
KoeusIss <sebri.issam@gmail.com>
2020-04-28 14:31:12 +0100

    Fix the use fo chr() function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 def uppercase(str):
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
-            cc = chr(ord(c) - 32)
+            cc = ord(c) - 32
         else:
-            cc = c
-        print("{}".format(cc), end='')
+            cc = ord(c)
+        print("{:c}".format(cc), end='')
     print(end='\n')
