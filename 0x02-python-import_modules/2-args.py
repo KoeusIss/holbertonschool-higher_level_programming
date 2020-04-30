dd6e40b8b087677deba91ebd12808ea1d44cc804
KoeusIss <sebri.issam@gmail.com>
2020-04-30 16:46:52 +0100

    commit the third task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,9 @@
 if __name__ == "__main__":
     import sys
     lst = sys.argv
+    lth = len(lst) - 1
     arg = "argument"
-    if len(lst) == 1:
-        print("{} {}.".format(len(lst) - 1, arg))
-    else:
-        if len(lst) > 1:
-            arg = "arguments"
-        print("{} {}: ".format(len(lst) - 1, arg))
-        for i in range(1, len(lst)):
-            print("{}: {}".format(i, lst[i]))
+    print("{} argument{}{}"
+            .format(lth, "" if lth == 1 else "s", "." if lth == 0 else ":"))
+    for i in range(1, len(lst)):
+        print("{}: {}".format(i, lst[i]))
