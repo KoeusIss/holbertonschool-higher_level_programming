dd599b1b30b4f213196af26be652b57216fd71f0
KoeusIss <sebri.issam@gmail.com>
2020-04-30 18:41:33 +0100

    fix the dir statement

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    lst = dir(hidden_4.pyc)
-    for i in lst:
+    for i in dir(hidden_4):
         if i[:2] != "__":
             print(i)
