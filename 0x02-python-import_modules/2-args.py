6757f133ee0e82d7c394d250998416f46339559e
KoeusIss <sebri.issam@gmail.com>
2020-04-30 16:00:43 +0100

    Add the first line arguments counter

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    argument = "argument"
+    if len(sys.argv) > 1:
+        argument = "arguments"
+    print("{} {}: ".format(len(sys.argv), argument))
     for i in range(0, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
