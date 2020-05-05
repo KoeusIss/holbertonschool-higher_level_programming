300afd7e381c359a52fc1470f78fb43ea94b2bf2
KoeusIss <sebri.issam@gmail.com>
2020-05-05 22:52:47 +0100

    Commit the advanceed task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <python3.8/Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	size_t size, i = 0;
+	PyObject *obj;
+
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %zu\n", size);
+	printf("[*] Allocated = %zu\n", ((PyListObject *)p)->allocated);
+	while (i < size)
+	{
+		obj = PyList_GET_ITEM(p, i);
+		printf("Element %zu: %s\n", i, Py_TYPE(obj)->tp_name);
+		i++;
+	}
+}
