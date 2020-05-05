84e7645a01f468df12138dcc33bc1b253e08f87c
KoeusIss <sebri.issam@gmail.com>
2020-05-05 12:50:10 +0100

    Fix the print format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,5 +9,5 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         Nothing, just print out the list reversely
     """
-    for i in my_list[::-1]:
-        print(i)
+    for item in my_list[::-1]:
+        print('{:d}'.format(item))
