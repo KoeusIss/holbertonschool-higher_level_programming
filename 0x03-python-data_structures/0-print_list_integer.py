484052f126a8c6351a64f5116c12e61c0ab76531
KoeusIss <sebri.issam@gmail.com>
2020-05-05 02:38:39 +0100

    Commit the second task

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    Prints all integer of a list
+
+    Args:
+        my_list: integer list
+
+    Returns:
+        Nothing, just print out the list
+    """
+    for i in my_list:
+        print(i)
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Rerieves element from a list
+
+    Args:
+        my_list: the given list
+        idx: the meant index position
+
+    Returns:
+        None: if idx is negative
+        None: if idx out of range
+        List item at idx: if succeed
+    """
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
