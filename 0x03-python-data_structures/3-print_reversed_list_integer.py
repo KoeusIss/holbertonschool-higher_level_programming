a5c1702e3c963e78794b67fb67299331fa6ff27e
KoeusIss <sebri.issam@gmail.com>
2020-05-05 14:12:52 +0100

    Fix the fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,8 +9,8 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         Nothing, just print out the list reversely
     """
-    if len(my_list) == 0:
-        print()
+    if not my_list:
+        return None
     my_list.reverse()
     for item in my_list:
         print('{:d}'.format(item))
