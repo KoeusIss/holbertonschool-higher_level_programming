02c90e617889a55a52c8f4b42911cd0eacabeabd
KoeusIss <sebri.issam@gmail.com>
2020-05-05 16:13:58 +0100

    Fix the style

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     firstchar = sentence[0] if sentence else None
-    sentlen =  len(sentence)
+    sentlen = len(sentence)
     return (sentlen, firstchar)
-
