f18988e909a004478dc8b7c2f5f8fc8843070b6d
KoeusIss <sebri.issam@gmail.com>
2020-05-05 16:19:43 +0100

    Fix the enmpty lisr

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_item = my_list[0]
     if not my_list:
         return None
+    max_item = my_list[0]
     for item in my_list:
         if item > max_item:
             max_item = item
