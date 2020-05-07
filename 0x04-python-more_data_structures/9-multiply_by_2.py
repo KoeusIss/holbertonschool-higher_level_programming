4953d7cc852c2682d18c40face72b1d5d7a154d4
KoeusIss <sebri.issam@gmail.com>
2020-05-07 05:05:14 +0100

    fix the operand

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     """
     Returns a new dictionary with all values multiplied by 2
     """
-    return {k: v**2 for k, v in a_dictionary.items()}
+    return {k: v*2 for k, v in a_dictionary.items()}
