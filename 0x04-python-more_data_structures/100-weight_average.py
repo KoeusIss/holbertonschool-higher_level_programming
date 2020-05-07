f356aa8d50e61c45d95da763add8cdae13b92467
KoeusIss <sebri.issam@gmail.com>
2020-05-07 16:27:59 +0100

    Fix the empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,5 +3,7 @@ def weight_average(my_list=[]):
     """
     Returns the weighted average of all integers tuple
     """
+    if not my_list:
+        return 0
     somme = sum([x * y for x, y in my_list])
     return  somme / sum([y for x, y in my_list])
