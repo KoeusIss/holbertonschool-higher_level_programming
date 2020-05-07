bdcd3ee8baaa902e9fd25c61bba0937b8eabc951
KoeusIss <sebri.issam@gmail.com>
2020-05-07 16:06:36 +0100

    Commit the twleveth task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman = {
+        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "DM": 900,
+        "M": 1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400
+    }
     if not isinstance(roman_string, str):
         return 0
     value = 0
-    last = ""
-    for c in roman_string:
-        if c not in roman:
-            return 0
-        elif (c == "V" or c == "X") and last == "I":
-            value += roman[c] - 2
+    c = 0
+    l = len(roman_string)
+    while c < l:
+        if c + 1 < l and roman_string[c:c + 2] in roman:
+            value += roman[roman_string[c:c + 2]]
+            c += 2
         else:
-            value += roman[c]
-        last = c
+            value += roman[roman_string[c]]
+            c += 1
     return value
