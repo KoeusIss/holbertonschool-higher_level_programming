5b9f779d750068a40e34c9565dc03cc725a99a1b
KoeusIss <sebri.issam@gmail.com>
2020-05-07 14:59:31 +0100

    Commit the therteenth taks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,15 @@
 def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if not roman_string.isalpha():
-        return None
+        return 0
     value = 0
+    last = ""
     for c in roman_string:
         if c not in roman:
-            return None
+            return 0
+        elif (c == "V" or c == "X") and last == "I":
+            value += roman[c] - 2
         else:
             value += roman[c]
+        last = c
     return value
