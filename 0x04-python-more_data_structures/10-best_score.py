74850ccc65cfa33ba84138f8aac82e2bf26be78c
KoeusIss <sebri.issam@gmail.com>
2020-05-07 14:35:10 +0100

    Fix the empty condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     """
     Returns key with the biggest integer value
     """
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary is {}:
         return None
     else:
         return max(a_dictionary)
