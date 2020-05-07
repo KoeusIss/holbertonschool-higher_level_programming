1d324e4afd564b0f7f5fb6f7429f2b5adb118672
KoeusIss <sebri.issam@gmail.com>
2020-05-07 15:08:36 +0100

    Fix the string checks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    if not roman_string.isalpha():
+    if not isinstance(roman_string, str):
         return 0
     value = 0
     last = ""
