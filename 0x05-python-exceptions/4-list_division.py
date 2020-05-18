a487b967a73852a6d76b9bcad3ba57d1a7840924
KoeusIss <sebri.issam@gmail.com>
2020-05-18 20:01:27 +0100

    commit the fifth task

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    Divides element by elements 2 list
+    """
+    result = []
+    for i in range(0, list_length):
+        try:
+            result.append(my_list_1[i] / my_list_2[i])
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        finally:
+            pass
+    return result
