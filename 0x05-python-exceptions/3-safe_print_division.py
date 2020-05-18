433a2f9726c62fab353104e1ef31fca95e73eb4f
KoeusIss <sebri.issam@gmail.com>
2020-05-18 18:30:38 +0100

    fix the style

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     try:
         result = a/b
     except:
-        result =  None
+        result = None
     finally:
         print('Inside result: {}'.format(result))
         return result
