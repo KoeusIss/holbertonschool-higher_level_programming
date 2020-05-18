5fbc3b193f3faef8e4caca012b9a080847a72c8e
KoeusIss <sebri.issam@gmail.com>
2020-05-18 20:17:51 +0100

    Commit the seventh task

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a message
+    """
+    try:
+        raise NameError(massage)
+    except:
+        raise
