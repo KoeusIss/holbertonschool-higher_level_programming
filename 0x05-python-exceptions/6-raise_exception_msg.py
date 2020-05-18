3b822e720f27352de72ffff53a0a6ce01c435ee0
KoeusIss <sebri.issam@gmail.com>
2020-05-18 20:19:11 +0100

    fix a style

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -4,6 +4,6 @@ def raise_exception_msg(message=""):
     Raises a name exception with a message
     """
     try:
-        raise NameError(massage)
+        raise NameError(message)
     except:
         raise
