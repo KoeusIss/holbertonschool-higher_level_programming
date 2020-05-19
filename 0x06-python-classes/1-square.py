65cdfe2e0f32755ef6624c4bdee50a25a3dde067
KoeusIss <sebri.issam@gmail.com>
2020-05-19 14:46:02 +0100

    Commit the ssecond task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square define a geometric shape square
+
+    Attributes:
+        __size (int): the size of the square
+
+    """
+    def __init__(self, size):
+        """
+        Init method is a constructor fo Square class
+
+        Args:
+            size (int): the size of the square
+
+        """
+        self.__size = size
