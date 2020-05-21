2d400a9aa4f901cdc436618b5f7df9e4def72b4f
KoeusIss <sebri.issam@gmail.com>
2020-05-21 20:32:50 +0100

    commit the fourth task - print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+PrintSquare module
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character '#'
+
+    Args:
+        size (int): the size length of the square
+
+    Raises:
+        TypeError: if size not an integer
+        ValueError: if size less than 0
+        TypeError: if size is float and less than 0
+
+    """
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size is 0:
+        print()
+    for row in range(size):
+        print('#'*size)
