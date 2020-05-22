b29345a5d0228a19a280df188c5b01a2a0678b46
KoeusIss <sebri.issam@gmail.com>
2020-05-22 16:11:59 +0100

    fix the test of the fifth task"

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -19,4 +19,12 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    pass
+
+    string = ""
+    for c in text:
+        string += c
+        if c in ":.?":
+            print(string.lstrip(), end="\n\n")
+            string = ""
+    print(string.lstrip(), end='')
+
