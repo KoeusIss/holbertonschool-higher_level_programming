496dae2d498a202e49059d4bbf459a8408d19151
KoeusIss <sebri.issam@gmail.com>
2020-06-02 16:37:14 +0100

    commit the task 6 - BaseGeometry with area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Base Geometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
