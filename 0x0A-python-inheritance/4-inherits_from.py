ed7a7495a9afdb52a433312b9dea9b84101a3432
KoeusIss <sebri.issam@gmail.com>
2020-06-02 18:35:20 +0100

    fix the second parameter

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
         (False) otherwise
 
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
