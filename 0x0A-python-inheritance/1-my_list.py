08c76da7d07f81992e9e36e1f406ed45aa7d31a6
KoeusIss <sebri.issam@gmail.com>
2020-06-02 15:53:51 +0100

    commit the task 1 - print sorted

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+MyList Module
+"""
+
+
+class MyList(list):
+    """MyList class"""
+
+    def print_sorted(self):
+        """Prints a sorted list"""
+        print sorted(self)
