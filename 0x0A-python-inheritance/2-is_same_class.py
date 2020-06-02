c91b05e83abcfaa676d0cbeb15cc1b3b8a1ee35b
KoeusIss <sebri.issam@gmail.com>
2020-06-02 16:08:39 +0100

    commit the task 2 - is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def is_same_class(obj, a_class):
+    """Returns if an object is an instance of the given class ou nn
+
+    Args:
+        obj (object): the given object
+        a_class (class): the given class
+
+    Returns:
+        (True) if the obj is an instance of a_class
+        (False) otherwise
+
+    """
+    return isinstance(obj, a_class)
