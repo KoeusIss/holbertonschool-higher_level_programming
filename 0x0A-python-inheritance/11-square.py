5ad271ff7f96300317491e6e8c55c91b6063d630
KoeusIss <sebri.issam@gmail.com>
2020-06-02 20:52:48 +0100

    commit the 11-square taské

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Square module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class
+
+    Attributes:
+        size (int): the size of the square
+
+    """
+    def __init__(self, size):
+        """Initializer"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the Square area"""
+        return self.__size**2
+
+    def __str__(self):
+        """Humain reading representation"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
