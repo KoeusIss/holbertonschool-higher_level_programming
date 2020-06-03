3bcbdda98c6a996344f6d4a755c5ec12ca4b5eb8
KoeusIss <sebri.issam@gmail.com>
2020-06-03 17:01:12 +0100

    fix the style doc of the task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""AddItem module"""
 import sys
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-"""Operations"""
 try:
     lst = load_from_json_file("add_item.json")
 except FileNotFoundError:
