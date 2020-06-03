dbc772e3da69b0f3cc546d80f82e1ff6a8e18bf0
KoeusIss <sebri.issam@gmail.com>
2020-06-03 13:24:40 +0100

    commit the task 6 - from_json_to_str

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""FromJsonToString module"""
+
+
+import json
+def from_json_string(my_str):
+    """Returns an object from json
+
+    Args:
+        my_str (str): the given json string
+
+    Returns:
+        (object): the object
+
+    """
+    return json.loads(my_str)
