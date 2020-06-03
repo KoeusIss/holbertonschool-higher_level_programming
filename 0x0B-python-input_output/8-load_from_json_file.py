4100719eef1cc84a9f1f72dddb7fb432f2bc7c6e
KoeusIss <sebri.issam@gmail.com>
2020-06-03 14:09:09 +0100

    commit the task 8 - load from json file

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""LoadFromJsonFile module"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates object from a JSON file
+
+    Args:
+        filename (str): a text file name
+
+    Returns:
+        (object): object newly created
+
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
