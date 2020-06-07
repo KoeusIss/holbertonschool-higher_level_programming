af62daf28c4d4ed4419eb814f73ee6feed33ca31
KoeusIss <sebri.issam@gmail.com>
2020-06-07 21:21:11 +0100

    first attempt to solve task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base Module"""
+import json
 
 
 class Base:
@@ -24,3 +25,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns JSON format representaion"""
+        return str(map(json.dumps, list_dictionaries))
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,6 +1,8 @@
 """TestBase module"""
 import unittest
 from models.base import *
+from models.rectangle import *
+from models.square import *
 
 
 class TestBase(unittest.TestCase):
@@ -12,16 +14,24 @@ class TestBase(unittest.TestCase):
         self.__b = Base(42)
 
     def test_creation_base_instance(self):
+        """test base instance creation"""
         self.assertIsInstance(self.__a, Base)
 
     def test_creation_base_instance_without_id_arg(self):
+        """test base instance creation without id"""
         self.assertEqual(self.__a.id, 3)
 
     def test_creation_two_base_instance_without_id_arg(self):
+        """test base instance creation without id"""
         self.assertEqual(self.__a.id, 4)
 
     def test_creation_base_instance_with_id_arg(self):
+        """test base instance creatin with given id"""
         self.assertEqual(self.__b.id, 42)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_returns_json_dictionary(self):
+        r = Rectangle(10, 7, 2, 8, 255)
+        dictionary = r.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        expected = [{"x": 2, "width": 10, "id": 255, "height": 7, "y": 8}]
+        self.assertEqual(json_dictionary, expected)
