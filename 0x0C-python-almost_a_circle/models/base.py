4e5c550cc503e9b8028c0144aacad652994f4a22
KoeusIss <sebri.issam@gmail.com>
2020-06-10 21:53:06 +0100

    fix the class name condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,10 +58,10 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        if cls is Rectangle:
-            dummy = Rectangle(1, 1)
-        elif cls is Square:
-            dummy = Square(1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
         else:
             dummy = None
         dummy.update(**dictionary)
