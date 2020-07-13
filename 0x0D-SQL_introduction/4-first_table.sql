f4e2290535396eca37e0ee87ddaeb0d669471226
KoeusIss <sebri.issam@gmail.com>
2020-07-13 12:02:10 +0100

    commit the task five: create first table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Task 0x04: Creates table in database of MySQL server
+-- Create tables first_table in a passed database
+CREATE TABLE IF NOT EXISTS `first_table` (
+    `id` INT,
+    `name` VARCHAR(256)
+);
