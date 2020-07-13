2d8acd2aae950c44208380ad4c0f34730ee02d2e
KoeusIss <sebri.issam@gmail.com>
2020-07-13 13:05:02 +0100

    fix John name

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,15 +1,16 @@
 -- Task 0x09: Crates a table in database in MySQL server
 -- Create table hbtn_0c_0.second_table
 CREATE TABLE IF NOT EXISTS `second_table`(
-    id INT,
-    name VARCHAR(256),
-    score INT
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
 );
+
 -- Insert multiple records to hbtn_0c_0.second_table
 INSERT INTO `second_table`
-    (id, name, score)
+    (`id`, `name`, `score`)
 VALUES
-    (1, "Jhon", 10),
+    (1, "John", 10),
     (2, "Alex", 3),
     (3, "Bob", 14),
     (4, "George", 8);
