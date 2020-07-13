28727a5de0ba12a63f294b0f9725c9d657d13db1
KoeusIss <sebri.issam@gmail.com>
2020-07-13 12:40:03 +0100

    add selection condition where id equal to 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,5 @@
 -- Task 0x08: Display the number of records in table in MySQL server
 -- Select count of rows in hbtn_0c_0.first_table
-SELECT COUNT(*) FROM `first_table`;
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id`=89;
