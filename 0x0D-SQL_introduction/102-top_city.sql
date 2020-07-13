e0c01d6d162397dcc94e82c139a3aa44c737e53e
KoeusIss <sebri.issam@gmail.com>
2020-07-13 14:50:17 +0100

    commit the third advanced task

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Task 0x12: Retrive temperatures average from MySQL server
+-- Select temperature avergae by city from hbtn_0c_0.temperatures in summer
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month=7 or month=8
+GROUP BY city
+ORDER BY AVG(value) DESC
+LIMIT 3;
