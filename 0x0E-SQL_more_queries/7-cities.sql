76ef53ed7f14e957840805334eaf93dfe3c2b804
KoeusIss <sebri.issam@gmail.com>
2020-07-14 21:05:34 +0100

    fix the inherit table in task seven

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,10 +5,9 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 
 -- Create table hbtn_0d_usa.cities
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-    `id`        INT NOT NULL UNIQUE AUTO_INCREMENT,
+    `id`        INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     `state_id`  INT NOT NULL,
     `name`      VARCHAR(256) NOT NULL,
-
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`state_id`) REFERENCES states(`id`)
+    FOREIGN KEY (`state_id`) REFERENCES hbtn_0d_usa.states(`id`)
 );
