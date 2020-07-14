e683c385baf8fcfd6d07a6e335556638fa186d23
KoeusIss <sebri.issam@gmail.com>
2020-07-14 20:56:22 +0100

    fix the left join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,9 +3,9 @@
 -- Lists all shows, and all genres linked to it
 SELECT      s.`title`, g.`name`
 FROM        `tv_shows` AS s
-            INNER JOIN `tv_show_genres` AS sg
+            LEFT JOIN `tv_show_genres` AS sg
                 ON s.`id` = sg.`show_id`
-                RIGHT JOIN `tv_genres` AS g
+                LEFT JOIN `tv_genres` AS g
                     ON sg.`genre_id` =  g.`id`
 ORDER BY    s.`title` ASC,
             g.`name` ASC;
