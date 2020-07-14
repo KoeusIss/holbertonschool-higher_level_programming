68ae770155f83ce8737a8715992813c53f307008
KoeusIss <sebri.issam@gmail.com>
2020-07-14 11:19:53 +0100

    commit the second task: ctreate and grent previleges to user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Task 0x01: Grant previlges in MySQL server
+
+-- Creates a user user_0d_1@localhost
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+            IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant all previleges to user_0d_1@localhost
+GRANT ALL PRIVILEGES
+                  ON *.*
+                  TO 'user_0d_1'@'localhost';
