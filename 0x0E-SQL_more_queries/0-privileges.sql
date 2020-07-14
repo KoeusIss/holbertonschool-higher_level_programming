9a35a440a66783f2567e9c58c9abf6ee1096b586
KoeusIss <sebri.issam@gmail.com>
2020-07-14 10:33:08 +0100

    commit the first task: checks the granted previleges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- Task 0x00: Grant previlges in MySQL server
+
+-- Lists all previleges of the user user_0d_1
+SHOW GRANTS
+        FOR 'user_0d_1'@'localhost';
+
+-- Lists all previleges of the user user_0d_2
+SHOW GRANTS
+        FOR 'user_0d_2'@'localhost';
