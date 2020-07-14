7873047a0da1f9a4f3cf98b39859848669a7a65c
KoeusIss <sebri.issam@gmail.com>
2020-07-14 14:02:26 +0100

    fix the the query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,12 @@
 -- Task 0x08: Queries in MySQL server
 
 -- Lists all cities in California
-SELECT      `cities`.`id`, `cities`.`name`
-FROM        `cities`, `states`
-WHERE       `states`.`name` = "California"
-ORDER BY    `cities`.`id`;
+SELECT DISTINCT `cities`.`id`, `cities`.`name`
+FROM            `cities`, `states`
+WHERE           `cities`.`state_id` =
+(
+    SELECT  `states`.`id`
+    FROM    `states`
+    WHERE   `states`.`name` = "California"
+)
+ORDER BY    `cities`.`id` ASC;
