f7d219ced536a37a851defdb7451d87d6709f7fd
KoeusIss <sebri.issam@gmail.com>
2020-07-14 12:25:39 +0100

    commit the task eight: create constraint

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- Task 0x07: Create table and database in MySQL server
+
+-- Create database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+-- Create table hbtn_0d_usa.cities
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    `id`        INT NOT NULL UNIQUE AUTO_INCREMENT,
+    `state_id`  INT NOT NULL,
+    `name`      VARCHAR(256) NOT NULL,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`state_id`) REFERENCES states(`id`)
+);
