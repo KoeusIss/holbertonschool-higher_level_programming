051f2cdeda3532552eb0dc76c8205c71cc7d5cfb
KoeusIss <sebri.issam@gmail.com>
2020-08-07 19:43:32 +0100

    commit the second task: filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+SelectStates module
+"""
+import MySQLdb
+import sys
+
+
+def filter_states():
+    """Filters states from database"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
+    cur = db.cursor()
+    cur.execute("SELECT *\
+                FROM states\
+                WHERE name LIKE 'N%'\
+                ORDER BY id ASC"
+                )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    filter_states()
