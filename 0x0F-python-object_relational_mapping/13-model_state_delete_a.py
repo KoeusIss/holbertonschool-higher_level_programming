e50a0420a34ca649606962a6af659294517c3b22
KoeusIss <sebri.issam@gmail.com>
2020-08-08 14:22:24 +0100

    commit the task 13: delete state

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+State module
+"""
+from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+
+
+def delete_state():
+    """Deletes states"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(
+            State.name.like('%a%')):
+        session.delete(state)
+
+    session.commit()
+    session.close()
+
+if __name__ == "__main__":
+    delete_state()
