86f0582533d1bc73ce6f015dd84ec7fea9a934ed
KoeusIss <sebri.issam@gmail.com>
2020-08-18 21:02:13 +0100

    fix the silent argument

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request
-curl "$1" -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
+curl "$1" -sX POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
