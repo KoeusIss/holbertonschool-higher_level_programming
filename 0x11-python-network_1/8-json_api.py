035c62b3d3e571e5fa63596989787870454dbfb8
KoeusIss <sebri.issam@gmail.com>
2020-08-24 21:43:55 +0100

    fix the data dictionnary passed to the post request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == '__main__':
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
-    r = requests.post('http://0.0.0.0:5000/search_user', q)
+    data = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
     try:
         if not r.json():
             print("No result")
