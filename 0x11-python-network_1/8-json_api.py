3850c24fe8607486e49d0e9ddbc6ee96586ee8f9
KoeusIss <sebri.issam@gmail.com>
2020-08-24 21:39:23 +0100

    fix some style

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,14 +5,12 @@ import sys
 
 
 if __name__ == '__main__':
-    q = sys.argv[1] or "" 
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     r = requests.post('http://0.0.0.0:5000/search_user', q)
     try:
         if not r.json():
             print("No result")
         else:
-            print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
+            print("[{}] {}".format(r.json().get("id"), r.json().get("name")))
     except ValueError:
         print("Not a valid JSON")
-
-
