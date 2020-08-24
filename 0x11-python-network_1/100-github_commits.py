202b4bfb614e9f25a0496c1cfc5aaaddf62806e5
KoeusIss <sebri.issam@gmail.com>
2020-08-24 23:08:07 +0100

    fix the request order

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,10 +5,8 @@ import sys
 
 
 if __name__ == '__main__':
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
     r = requests.get('https://api.github.com/repos/{}/{}/commits'.
-                     format(repo_name, owner_name))
+                     format(sys.argv[2], sys.argv[1]))
     for c in r.json()[0:10]:
         print("{}: {}".format(c.get('sha'),
                               c.get('commit').get('author').get('name')))
