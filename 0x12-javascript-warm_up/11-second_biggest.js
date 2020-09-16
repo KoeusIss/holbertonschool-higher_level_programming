6a51de06a94448eed53712923f792b63af17d6f0
Sebri Issam <sebri.issam@gmail.com>
2020-09-16 21:34:06 +0100

    fix the sort funcion compare

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,11 @@
 const array = process.argv.slice(2).map(function (x) {
   return Number(x);
 });
+array.sort(function (a, b) {
+  return a - b;
+});
 if (array.length > 1) {
-  console.log(array.sort()[array.length - 2]);
+  console.log(array[array.length - 2])
 } else {
   console.log(0);
 }
