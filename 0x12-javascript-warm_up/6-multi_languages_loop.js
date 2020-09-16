731be99c035c874eed96aa9f11d7a456d9beb772
Sebri Issam <sebri.issam@gmail.com>
2020-09-16 20:22:28 +0100

    commit the seventh task

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  console.log(element);
+}
